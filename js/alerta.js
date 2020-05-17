45855eea5b101ee3343d74f8dcd0746f867123ab
jeng <jamespaulespea@gmail.com>
2020-05-17 16:47:54 +0800

    add if and alerta

---
+++ js/alerta.js
@@ -0,0 +1,4 @@
+if (window.orientation === 90 || window.orientation === -90){
+    alert("Apologies but This is not optimize to work on a landscape view.\nPlease revert it back to Portrait.");
+    location.reload();
+}  // orientation
