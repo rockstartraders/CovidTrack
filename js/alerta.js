92616e700cf27319561c1a2687e058b77afb068d
jeng <jamespaulespea@gmail.com>
2020-05-17 17:24:05 +0800

    add if and alerta 2

--- js/alerta.js
+++ js/alerta.js
@@ -1,4 +1,18 @@
-if (window.orientation === 90 || window.orientation === -90){
-    alert("Apologies but This is not optimize to work on a landscape view.\nPlease revert it back to Portrait.");
-    location.reload();
-}  // orientation
+
+
+window.addEventListener("orientationchange", function() {                   
+    if (window.matchMedia("(orientation: portrait)").matches) {
+       
+     }
+    if (window.matchMedia("(orientation: landscape)").matches) {
+      alert("Apologies but This is not optimize to work on a landscape view.Please revert it back to Portrait.");
+     }
+}, false);
+
+
+
+//function isLandscape() {
+ //   return (window.orientation === 90 || window.orientation === -90);
+  //  alert("Apologies but This is not optimize to work on a landscape view.Please revert it back to Portrait.");
+  //  location.reload();
+//}
